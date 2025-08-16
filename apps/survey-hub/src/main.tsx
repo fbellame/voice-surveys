e1e1fa5ae28e028a4ec5cd65ee49990e5e390c9f
Farid Bellameche <farid.bellameche@desjardins.com>
2025-08-16 13:42:06 -0400

    Update shared package dependencies and configurations

--- apps/survey-hub/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import type { Campaign } from '@shared/survey';
-
-const App = () => <div>Survey Hub ready</div>;
-
-createRoot(document.getElementById('root')!).render(<App/>);
